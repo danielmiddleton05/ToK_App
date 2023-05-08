3ad90de048451afee07923c99abaadc47ec8995d
danielmiddleton05 <119559669+danielmiddleton05@users.noreply.github.com>
2023-05-08 10:46:13 +0900

    updated json

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>GET STARTED DUMMY</Text>
       <StatusBar style="auto" />
     </View>
   );
