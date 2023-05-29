1f6d9c271d7b682f365a4e1b6f10d6f08ed4c68f
danielmiddleton05 <119559669+danielmiddleton05@users.noreply.github.com>
2023-05-29 11:49:08 +0900

    stared, though it's a mess

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from './screens/MainComponent';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>GET STARTED DUMMY</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <NavigationContainer>
+                <Main />
+                <Header />
+                <Text style={styles.text}>GET STARTED DUMMY</Text>
+            </NavigationContainer>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fafafa',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
